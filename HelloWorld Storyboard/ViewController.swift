9ca5f0005849a12e2da218a5eceaf151483a37fd
D7703_07 <d7703_07@d7703-07ui-imac.local>
2018-03-07 15:14:56 +0900

    Initial Commit

---
+++ HelloWorld Storyboard/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HelloWorld Storyboard
+//
+//  Created by 김종현 on 2018. 3. 5..
+//  Copyright © 2018년 김종현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("Button pressed!!")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
