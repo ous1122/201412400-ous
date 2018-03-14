040f13fbb0f73d209c911c01133f9cc12764096b
D7703_13 <d7703_13@D7703-13ui-iMac.local>
2018-03-14 15:43:30 +0900

    135

--- changeButton/changeButton/ViewController.swift
+++ changeButton/changeButton/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 var a = 1
+    @IBOutlet weak var colorlb: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        view.backgroundColor = UIColor.yellow
+        view.backgroundColor = UIColor.blue
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,22 +26,48 @@ var a = 1
     @IBAction func btchange(_ sender: UIButton) {
         if a == 1{
             view.backgroundColor = UIColor.yellow
+            colorlb.text = "yellow"
+            colorlb.textColor = UIColor.black
             a += 1
         }
         else if a == 2
         {
             view.backgroundColor = UIColor.red
+            colorlb.text = "red"
             a += 1
         }
         else if a == 3{
             view.backgroundColor = UIColor.gray
+            colorlb.text = "gray"
             a += 1
         }
         else if a == 4{
             view.backgroundColor = UIColor.cyan
+            colorlb.text = "cyan"
             a = 1
         }
     }
     
+    @IBAction func btswitch(_ sender: UIButton) {
+        switch a {
+        case 1:
+            view.backgroundColor = UIColor.yellow
+            colorlb.text = "yellow"
+            a += 1
+        case 2:
+            view.backgroundColor = UIColor.red
+            colorlb.text = "red"
+            a += 1
+        case 3:
+            view.backgroundColor = UIColor.gray
+            colorlb.text = "gray"
+            a += 1
+        default:
+            view.backgroundColor = UIColor.cyan
+            colorlb.text = "cyan"
+            a = 1
+        }
+
+    }
 }
 
