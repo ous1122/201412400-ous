b6f50a1a159338fa9e95eb3b08bc3f41a56df804
D7703_13 <d7703_13@D7703-13ui-iMac.local>
2018-03-14 15:03:30 +0900

    123

---
+++ backgroundChange/backgroundChange/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  backgroundChange
+//
+//  Created by D7703_13 on 2018. 3. 14..
+//  Copyright © 2018년 gyuminpark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var a = 1
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.yellow
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btchange(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
---
+++ changeButton/changeButton/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  changeButton
+//
+//  Created by D7703_13 on 2018. 3. 14..
+//  Copyright © 2018년 gyuminpark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+var a = 1
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.yellow
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btchange(_ sender: UIButton) {
+        if a == 1{
+            view.backgroundColor = UIColor.yellow
+            a += 1
+        }
+        else if a == 2
+        {
+            view.backgroundColor = UIColor.red
+            a += 1
+        }
+        else if a == 3{
+            view.backgroundColor = UIColor.gray
+            a = 1
+        }
+    }
+    
+}
+
