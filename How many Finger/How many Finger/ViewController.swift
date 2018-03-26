fbda5800000c6b6ecb83512221ca0b9eb94d27a5
D7703_13 <d7703_13@d7703-13ui-imac.local>
2018-03-26 14:56:08 +0900

    fingers

---
+++ How many Finger/How many Finger/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  How many Finger
+//
+//  Created by D7703_13 on 2018. 3. 26..
+//  Copyright © 2018년 gyuminpark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var setNum: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func guess(_ sender: UIButton) {
+        //랜덤 숫자 생성하기
+        let number = arc4random() % 6
+        let numberString = String(number)
+        
+        if setNum.text == numberString {
+            result.text = "맞추었습니다."
+        }
+        else {
+            result.text = "틀렸습니다. 다시 시도하세요. 이번 문제의 숫자는 \(number) 입니다."
+        }
+        setNum.resignFirstResponder()
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        setNum.resignFirstResponder()
+            return true
+            
+            }
+        }
+    
+}
+
